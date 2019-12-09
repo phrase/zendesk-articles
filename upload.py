ebb234d7934002a6e46fc0d110f48a8945848468
Michal Kebrt <michal.kebrt@memsource.com>
2019-12-09 14:56:09 +0100

    article sync

--- upload.py
+++ upload.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python3
 
-import requests
 import json
-import time
-import pathlib
 import os
 import sys
 import time
 
+import requests
+
 import auth
 
 article_limit = None
-if (len(sys.argv) > 1):
+if len(sys.argv) > 1:
     article_limit = sys.argv[1]
-    
 
 rootDir = 'html'
 for dirName, subdirList, fileList in os.walk(rootDir):
     for fname in fileList:
         article, extension = os.path.splitext(fname)
-        if (extension == '.html'):
-            if (article_limit == None or article_limit == article):
-                url = 'https://memsource.zendesk.com/api/v2/help_center/articles/{}/translations/en-us.json'.format(article)
+        if extension == '.html':
+            if article_limit is None or article_limit == article:
+                url = 'https://memsource.zendesk.com/api/v2/help_center/articles/{}/translations/en-us.json'.format(
+                    article)
                 print(f"Requesting {url}")
                 headers = {'Content-Type': 'application/json'}
                 f = open(f"{dirName}/{fname}", "r")
-                data = { "translation": { "body": f.read() }}
+                data = {"translation": {"body": f.read()}}
                 response = requests.put(url, auth=auth.get_auth(), headers=headers, data=json.dumps(data))
                 print(response)
                 time.sleep(0.1)
