e86e7f9f84617d5d8d4a5f858a9c1274344dca3d
Michal Kebrt <michal.kebrt@memsource.com>
2019-12-11 12:13:48 +0100

    Create tampermonkey.js

---
+++ scripts/tampermonkey.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Zendesk articles at GitHub
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  show a link to Zendesk article
+// @author       You
+// @match        https://github.com/Memsource/zendesk-articles/blob/*.html
+// @grant        none
+// @require      http://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var $path = $('.final-path');
+    console.log($path);
+    var id = $path.text().split(".")[0];
+    var link = "https://help.memsource.com/hc/en-us/articles/" + id;
+    $path.html("<a target='_blank' href='" + link + "'>Zendesk: " + $path.text() + "</a>");
+})();
